aa8d4e0d23ae59d913f31acf011674040958ac74
rdy07ua <spartakadoma.net@gmail.com>
2023-06-07 18:36:17 +0200

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your text!");
+        String string = scanner.nextLine();
+        System.out.println(string.toLowerCase());
+        System.out.println(string.toUpperCase());
+        System.out.println(string.charAt(0));
+        System.out.println(string.charAt(string.length() - 1));
+    }
+
+
+    }
+
+/*
+
+        String zmina = new String("Text for changes");
+        System.out.println(zmina.toLowerCase());
+        System.out.println(zmina.toUpperCase());
+        System.out.println(zmina.substring(0, 1));
+        System.out.println(zmina.charAt(zmina.length() - 1));
+
+
     }
 }
+*/
